13937125145bebfb5ab7a0826f86cde6cb30da29
Max <maksymkharuk@iesebre.com>
2016-04-06 20:27:26 +0200

    Added image logo generation, improved code

--- resources/views/invoices/invoice_generator.blade.php
+++ resources/views/invoices/invoice_generator.blade.php
@@ -5,33 +5,71 @@
 @endsection
 
 @section('custom_scripts')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.2.61/jspdf.min.js"></script>
+
+        <!-- USING LATEST VERSION, NEED PNG PLUGINS FOR PNG SUPPORT -->
+    <!--script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.2.61/jspdf.min.js"></script-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.0.272/jspdf.min.js"></script>
 
     <script>
 
-        //var doc = new jsPDF();
-        //doc.text(20, 20, 'Hello world.');
-        //doc.save('Test.pdf');
+        // Image selector
+        image = $('#image');
+        img_data = {};
+
+        // Image selector listener
+        image.change(function() {
+
+            var user_file = image[0].files[0];
+
+            img_data = {
+                type: user_file.type === 'image/jpeg' ? 'JPEG' : 'PNG'
+            };
+
+            var reader = new FileReader();
+
+            reader.onload = function(event) {
+
+                img_data.src = event.target.result;
+                updatePdf();
+            };
+
+            reader.readAsDataURL(user_file);
+        });
 
 
-        var preview_container = $('#pdf_preview');
-        var update_preview_button = $('#refresh_btn');
-        var download_button = $('#download_btn');
 
 
-
-        //preview_container.attr('src', pdf.output('datauristring'));
-
-
-        updatePdf = function () {
-
-            var pdf = new jsPDF('p', 'mm', 'a4');
+        function generatePdf()
+        {
+            pdf = new jsPDF('p', 'mm', 'a4');
 
             pdf.text($('#title').val(), 20, 20);
             pdf.text($('#description').val(), 20, 40);
 
-            preview_container.attr('src', pdf.output('datauristring'));
+            if (img_data.src != null) {
+                pdf.addImage(img_data.src, img_data.type, 180, 10, 20, 20);
+            }
+
+            return pdf;
         }
+
+
+        function downloadPdf() {
+
+            pdf = generatePdf();
+
+            pdf.save("generated.pdf");
+        }
+
+
+        function updatePdf() {
+
+            pdf = generatePdf();
+
+            $('#pdf_preview').attr('src', pdf.output('datauristring'));
+        }
+
+
 
         updatePdf();
 
@@ -56,18 +94,20 @@
                         <label>Title</label>
                         <input id="title" class="form-control" placeholder="Invoice title" onblur="updatePdf()">
                     </div>
+
                     <div class="form-group">
                         <label>Description</label>
                         <textarea id="description" class="form-control" placeholder="Invoice description" rows="3" onblur="updatePdf()"></textarea>
                     </div>
-                    <!--div class="form-group">
+
+                    <div class="form-group">
                         <label>Logo</label>
-                        <input id="image" type="file">
-                    </div-->
+                        <input id="image" type="file" tabindex="1">
+                    </div>
 
                     <div class="form-group">
                         <button id="refresh_btn" class="btn btn-large btn-success" onclick="updatePdf()">Refresh</button>
-                        <button id="download_btn" class="btn btn-large btn-primary">Download</button>
+                        <button id="download_btn" class="btn btn-large btn-primary" onclick="downloadPdf()">Download</button>
                     </div>
 
 
