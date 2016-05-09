7f4b7b007654363f8e7726466db0e050d21ccaff
Max <maksymkharuk@iesebre.com>
2016-05-09 18:27:32 +0200

    Start implementing Redis

---
+++ node/broadcast_server/socket.js
@@ -0,0 +1,26 @@
+//Initialitze library requirements: express, socket.io and ioredires
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Redis = require('ioredis');
+
+//Create new Redis instance
+var redis = new Redis();
+
+//Example howto subscribe to only one Redis channel
+//redis.subscribe('test-channel', function(err, count) {
+//});
+
+//Subscribe to all Redis Channels
+redis.psubscribe('*', function(err, count) {
+    //Nothing to do here?
+    console.log('Errors subscribing to channel');
+});
+
+//Broadcast message when recieved from Redis on all channels
+redis.on('pmessage', function(subscribed,channel, message) {
+    console.log('Message Recieved at channel(' + channel + '): ' + message);
+    message = JSON.parse(message);
+    io.emit(channel + ':' + message.event, message.data);
+});
+
